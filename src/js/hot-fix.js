f8670ca53e8cb92a9e40dd4b5e94b99e197cc7b7
jun <2949195453@qq.com>
2022-05-29 12:49:00 +0800

    修改

--- src/js/hot-fix.js
+++ src/js/hot-fix.js
@@ -3,7 +3,8 @@ let master = "我是master的test";
 console.log(master);
 let hotFix = "我是hot-fix的test";
 console.log(hotFix);
-setTimeout(() => {
-    console.log("等待合并中...");
-},3000);
+// setTimeout(() => {
+//     console.log("等待合并中...");
+// },3000);
 console.log("我在远程库修改了。");
+console.log("我在本地库修改了。");
